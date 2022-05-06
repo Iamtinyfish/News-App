bf70beafe876f1b68b5d882b54b1128d3636795d
xuanhiep2k <tranxuanhiepcmd3@gmail.com>
2022-05-06 11:25:11 +0700

    update view

--- app/src/main/java/com/mad12/newsapp/ui/articleContent/ArticleContentActivity.java
+++ app/src/main/java/com/mad12/newsapp/ui/articleContent/ArticleContentActivity.java
@@ -5,16 +5,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.mad12.newsapp.MainActivity;
 import com.mad12.newsapp.R;
 import com.mad12.newsapp.databinding.ActivityArticleContentBinding;
 import com.mad12.newsapp.model.Article;
+import com.mad12.newsapp.ui.search.SearchActivity;
 import com.mad12.newsapp.utils.RetrofitClient;
 import com.squareup.picasso.Picasso;
 
@@ -34,11 +38,17 @@ public class ArticleContentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = ActivityArticleContentBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_article_content);
 
-        // show back button
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ArticleContentActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         //get article id from intent
         Intent intent = this.getIntent();
@@ -46,24 +56,11 @@ public class ArticleContentActivity extends AppCompatActivity {
         if (intent != null) articleId = intent.getStringExtra("article_id");
         getArticleById(articleId);
 
-//        Log.println(Log.DEBUG,"articleId", (articleId != null) ? articleId : "null");
-
-    }
-
-    //back button action
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
     }
-    //
 
     //Call API
     private void getArticleById(String id) {
